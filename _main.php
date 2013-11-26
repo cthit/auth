238d8035e525e1ede685cb306a624a68826c879a
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-11-26 21:38:42 +0100

    Hide form if signed in

--- _main.php
+++ _main.php
@@ -7,17 +7,19 @@
   <div class="col-lg-4">
     <p></p>
   </div>
-  <div class="col-lg-5 col-lg-offset-3">
-   <form role="form" class="form-horizontal" method="post" action="/auth/login.php">
-    <?php 
-      form_control("username", "CID", "input", "user", true);
-      form_control("password", "Lösenord", "password", "lock");
-    ?>
-    <div class="form-group">
-      <div class="col-lg-offset-2 col-lg-10">
-        <button type="submit" class="btn btn-primary" name="intent" value="login">Logga in</button>
+  <?php if (!is_signed_in()): ?>
+    <div class="col-lg-5 col-lg-offset-3">
+     <form role="form" class="form-horizontal" method="post" action="/auth/login.php">
+      <?php
+	form_control("username", "CID", "input", "user", true);
+	form_control("password", "Lösenord", "password", "lock");
+      ?>
+      <div class="form-group">
+	<div class="col-lg-offset-2 col-lg-10">
+	  <button type="submit" class="btn btn-primary" name="intent" value="login">Logga in</button>
+	</div>
       </div>
+     </form>
     </div>
-   </form>
-  </div>
+  <?php endif; ?>
 </div>
