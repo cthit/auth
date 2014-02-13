e03cf837b1d9dfd7c095eea5de07da4ba5d09307
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-02-13 20:34:35 +0100

    jsonp support

--- userInfo.php
+++ userInfo.php
@@ -1,17 +1,17 @@
 <?php
 
 if(!isset($_POST['token'])) {
-        if(!isset($_GET['token'])) {
-        		if (!isset($_COOKIE['chalmersItAuth'])) {
-                	die('Need more params, read the documentation for more info');
-                } else {
-                	$token = htmlentities($_COOKIE['chalmersItAuth']);
-                }
-        } else {
-                $token = htmlentities($_GET['token']);
-        }
+	if(!isset($_GET['token'])) {
+		if (!isset($_COOKIE['chalmersItAuth'])) {
+			die('Need more params, read the documentation for more info');
+		} else {
+			$token = htmlentities($_COOKIE['chalmersItAuth']);
+		}
+	} else {
+		$token = htmlentities($_GET['token']);
+	}
 } else {
-        $token = htmlentities($_POST['token']);
+	$token = htmlentities($_POST['token']);
 }
 
 require_once('ldap.php');
@@ -23,8 +23,11 @@ $ldap = new ldap('***REMOVED***');
 $tokenUser =  $auth->getUsername($token);
 if(!$tokenUser['username']) {
 	die('invalid token');
+} else if (isset($_GET['callback']) && $callback = $_GET['callback']) {
+	header("Content-Type: application/javascript");
+	echo "$callback(" . json_encode($ldap->search('(uid='.$tokenUser['username'].')')) . ");";
+} else {
+	header("Content-Type: application/json");
+	echo json_encode($ldap->search('(uid='.$tokenUser['username'].')'));
 }
-
-header("Content-Type: application/json");
-echo json_encode($ldap->search('(uid='.$tokenUser['username'].')'));
 
