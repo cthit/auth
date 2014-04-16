494a910577baa8da40d702b5cbc1ba7a2b2d7f6b
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-04-16 14:28:09 +0200

    refactoring and added support for finding by cid

--- userInfo.php
+++ userInfo.php
@@ -1,17 +1,11 @@
 <?php
-
-if(!isset($_POST['token'])) {
-	if(!isset($_GET['token'])) {
-		if (!isset($_COOKIE['chalmersItAuth'])) {
-			die('Need more params, read the documentation for more info');
-		} else {
-			$token = htmlentities($_COOKIE['chalmersItAuth']);
-		}
-	} else {
-		$token = htmlentities($_GET['token']);
-	}
-} else {
-	$token = htmlentities($_POST['token']);
+function if_isset($map, $key = 'token') {
+	return isset($map[$key]) && htmlentities($map[$key]);
+}
+$token = if_isset($_POST) || if_isset($_GET) || if_isset($_COOKIE, 'chalmersItAuth') || false;
+$cid = if_isset($_GET, 'cid') || false;
+if (!$token && !$cid) {
+	die('Need more params, read the documentation for more info');
 }
 
 require_once('ldap.php');
@@ -20,14 +14,21 @@ require_once('auth.php');
 $auth = new auth();
 $ldap = new ldap('***REMOVED***');
 
-$tokenUser =  $auth->getUsername($token);
-if(!$tokenUser['username']) {
-	die('invalid token');
-} else if (isset($_GET['callback']) && $callback = $_GET['callback']) {
+$user = false;
+if ($token) {
+	$tokenUser = $auth->getUsername($token);
+	$user = $tokenUser && $tokenUser['username'];
+	if (!$user) {
+		die('invalid token');
+	}
+}
+
+$json = json_encode($ldap->search_by_cid($user));
+if (isset($_GET['callback']) && $callback = $_GET['callback']) {
 	header("Content-Type: application/javascript");
-	echo "$callback(" . json_encode($ldap->search('(uid='.$tokenUser['username'].')')) . ");";
+	echo "$callback($json);";
 } else {
 	header("Content-Type: application/json");
-	echo json_encode($ldap->search('(uid='.$tokenUser['username'].')'));
+	echo $json;
 }
 
